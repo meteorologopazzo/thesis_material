600d209e0903c158735539ad00fc092689a43550
meteorologopazzo <miscellaneasuper@gmail.com>
2025-02-07 16:33:43 +0100

    Ago's code working with my rendering

--- EURECA_scripts/ABL_BULK/neggers_stevens.py
+++ EURECA_scripts/ABL_BULK/neggers_stevens.py
@@ -1,3 +1,36 @@
+import numpy as np
+
+# Define some meteorological functions from pycoare.
+def qsat(t,p):
+    """
+    usage: es = qsat(t,p)
+    Returns saturation vapor pressure es (mb) given t(C) and p(mb).
+    After Buck, 1981: J.Appl.Meteor., 20, 1527-1532
+    Returns ndarray float for any numeric object input.
+    """
+    from numpy import copy, asarray, exp
+
+    t2 = copy(asarray(t, dtype=float))  # convert to ndarray float
+    p2 = copy(asarray(p, dtype=float))
+    es = 6.1121 * exp(17.502 * t2 / (240.97 + t2))
+    es = es * (1.0007 + p2 * 3.46e-6)
+    return es
+
+def qsea(sst,p):
+    """
+    usage: qs = qsea(sst,p)
+    Returns saturation specific humidity (g/kg) at sea surface
+    given sst(C) and p(mb) input of any numeric type.
+    Returns ndarray float for any numeric object input.
+    """
+    ex = qsat(sst,p) # returns ex as ndarray float
+    es = ex#0.98 * ex # This is the correction for the effect of salinity, which we remove now
+    qs = 622*es/(p-0.378*es) # saturation specific humidity
+    return qs
+
+
+
+
 # Version 1.0 released by David Romps on September 12, 2017.
 # Version 1.1 vectorized lcl.R, released on May 24, 2021.
 # 
@@ -125,12 +158,31 @@ def lcl(p,T,rh=None,rhl=None,rhs=None,return_ldl=False,return_min_lcl_ldl=False)
         return lcl
         
         
-        
-def neggers_et_al_2006(t,y,V,SST,D,q_free,th_free,ps):
+def prt_dyer74(zL):
+    """
+    Computation of the turbulent Prandtl number as a function of the nondimensional height z/L (stability)
+    following the formulation of Dyer (1974) as in Li, Atmospheric Research (2019) 
+    https://doi.org/10.1016/j.atmosres.2018.09.015
+    """
+    if zL>0:
+        return 1
+    else:
+        return 1/(1-16*zL)**0.25
+    
+    
+
+    
+    
+def neggers_et_al_2006_stevens_et_al_2002(t,y,SST,D,q_free,th_free,ps,f,U_free,V_free,we, dict_ext):
     """
     Subscript s denotes surface values, b stands for buoyancy, which is associated with the virtual potential
     temperature (theta_v), th indicates the liquid water potential temperature (theta_l) and q the total specific
     humidity (q_t).
+    A bulk model of the wind speed is added starting from the Stevens et al., JCli (2002) paper
+    "Entrainment, Rayleigh friction, and boundary layer winds over the tropical Pacific"
+    y[3] is U, the zonal bulk wind component
+    y[4] is V, the meridional bulk wind component
+    V_mag is the magnitude of the bulk wind = np.sqrt(y[3]**2+y[4]**2)
     """
     # Define some constants.
     delta_z = 100.0 # [m]
@@ -145,6 +197,7 @@ def neggers_et_al_2006(t,y,V,SST,D,q_free,th_free,ps):
     beta = 0.36
     gamma = 1.55
     g = 9.81 # [m/s2], acceleration due to gravity
+    CD = 1/900. # drag coefficient.
     
     ### Define some forcing variables for the BOMEX example -> this will be given as input parameters.
     #V = 8.75 # [m/s], large scale horizontal wind
@@ -162,65 +215,56 @@ def neggers_et_al_2006(t,y,V,SST,D,q_free,th_free,ps):
     
     ### Define some diagnostic variables.
     # Entrainment.
-    q_flux_s = V*Cqs*(q_s-y[1]) # total specific humidity flux at the surface
-    q_flux_s_CC = V*Cqs*(q_s) # Clausius Clapeyron specific humidity flux at the surface
-    th_flux_s = V*Cths*(th_s-y[2]) # liquid water potential temperature flux at the surface
-    thv_flux_s = (1+0.61*q_s)*th_flux_s + 0.61*th_s*q_flux_s # surface virtual potential temp flux
-    #thv_flux_s = (1+0.61*y[1])*th_flux_s + 0.61*y[2]*q_flux_s # surface virtual potential temp flux
+    V_mag = np.sqrt(y[3]**2+y[4]**2) # [m/s], bulk wind speed.
+    q_flux_s = V_mag*Cqs*(q_s-y[1]) # total specific humidity flux at the surface
+    th_flux_s = V_mag*Cths*(th_s-y[2]) # liquid water potential temperature flux at the surface
+    thv_flux_s = (1+0.61*q_s)*th_flux_s + 0.61*th_s*q_flux_s # surface virtual potential temp flux, <w'theta_v'>    # Entrainment.
     delta_q = Cqc*(q_free-y[1])
     delta_th = Cthc*(th_free-y[2])
     delta_thv = delta_th + 0.61*(y[1]*delta_th + y[2]*delta_q + delta_q*delta_th)
     E = 0.2*thv_flux_s/delta_thv
-    
-#     A simple parameterization of cloud water related variables for use in boundary layer models
     # Mass flux.
-    #thv0 = y[2]*(1+0.61*y[1]) # [K], ABL virtual potential temperature
-    thv0 = th_s*(1+0.61*q_s) # [K], Surface virtual potential temperature
+    thv0 = y[2]*(1+0.61*y[1]) # [K], ABL virtual potential temperature
     b_flux_s = g*thv_flux_s/thv0 # surface buoyancy flux
     w_star = (y[0]*b_flux_s)**(1/3) # m/s, Deardorff convective velocity scale. 5th try
 #    w_star = (g*y[0]*thv_flux_s/(y[2]*(ps/ref_p)**(Rd/cpd)*(1+0.61*y[1])))**(1/3) # m/s, Deardorff convective velocity scale. 4th try
 #    w_star = (g*y[0]*thv_flux_s/(y[2]*(1+0.61*y[1])))**(1/3) # m/s, Deardorff convective velocity scale. 3rd try
 #    w_star = (g*y[0]*thv_flux_s/y[2])**(1/3) # m/s, Deardorff convective velocity scale. 2nd try
-#    w_star = (g*y[0]*thv_flux_s/th_s)**(1/3) # m/s, Deardorff convective velocity scale. 1st try
-    
+#    w_star = (g*y[0]*thv_flux_s/th_s)**(1/3) # m/s, Deardorff convective velocity scale. 1st try    
     T0 = y[2]*(ps/ref_p)**(Rd/cpd) # [K], air temperature at the surface, from the ABL theta value.
     T_h = T0-g/cpd*y[0] # [K], air temperature at h following a dry adiabat.
     p_h = ps*100*(1-g*y[0]/(T0*cpd))**(cpd/Rd) # [Pa], air pressure at h with p=rho*R*T; dp/dz=-rho*g; dtheta/dz=0 
     e_sat_h = qsat(T_h-273.15,p_h/100)*100 # [Pa], saturation vapor pressure at h.
-    q_sat = 0.622*e_sat_h/(p_h-0.378*e_sat_h) # [kg/kg], saturation specific humidity at h.
-   
+    q_sat = 0.622*e_sat_h/(p_h-0.378*e_sat_h) # [kg/kg], saturation specific humidity at h.   
     sigma_q = np.sqrt(-q_flux_s*delta_q*y[0]/(w_star*delta_z))
     area_c = 0.5 + beta*np.arctan(gamma*(y[1]-q_sat)/sigma_q)
     M = area_c*w_star
-    
-    # Define here the mass flux used in Neggers et al. (2019) (also from Naumann et al. 2019) computing the LCL with the exact formula from Romps (2017)
-    tau = 15*60 # adjustment time scale = 15 min
-    LCL = lcl(ps*100,T0,y[1]/q_s)
-    #M = np.heaviside(y[0]-LCL,0)*(y[0]-LCL)/tau
-
-    # Surface air density.
-    rhos = ps*100/(Rd*T0)
+    # Entrainment for the horizontal momentum.
+    u_star = V_mag*np.sqrt(CD) # [m/s], Friction velocity, assuming air density equal to 1... We can correct this!
+    L = - u_star**3/(0.4*b_flux_s)# [m], Monin-Obukhov length
+#     print
+    zL = 10/L # [1], ratio z/L, we assume z=10 m as a reference height: we are interested in the surface stability
+    we_dyn = E*prt_dyer74(zL)
+#     print(we_dyn)
     
     ### Define the equations to be solved.
     dh_dt = E - D*y[0] - M 
     dq_dt = (q_flux_s + E*delta_q)/y[0] + F_advq
     dth_dt = (th_flux_s + E*delta_th)/y[0] + F_advth + F_rad
+    dU_dt = f*(y[4]-V_free)-y[3]*(CD*V_mag+we_dyn)/y[0]+U_free*we_dyn/y[0]
+    dV_dt = -f*(y[3]-U_free)-y[4]*(CD*V_mag+we_dyn)/y[0]+V_free*we_dyn/y[0]
     
     #print(area_c)
     #print((y[1]-q_sat)*1e3)
     #print(sigma_q*1e3)
     #print(w_star)
     #print((y[1]-q_sat)/sigma_q)
-    area_c_ext.append(area_c)
-    w_star_ext.append(w_star)
-    M_ext.append(M)
-    E_ext.append(E)
-    sigma_q_ext.append(sigma_q)
-    LHF_ext.append(q_flux_s*Le*rhos)
-    LHF_CC_ext.append(q_flux_s_CC*Le*rhos)
-    SHF_ext.append(th_flux_s*cpd*rhos)
-    LCL_ext.append(LCL)
-    qs_ext.append(q_s)
-    qsat_ext.append(q_sat)
     
-    return dh_dt, dq_dt, dth_dt
+    dict_ext['area_c_ext'].append(area_c)                 # area_c_ext
+    dict_ext['w_star_ext'].append(w_star)                 # w_star_ext
+    dict_ext['M_ext'].append(M)                      # M_ext
+    dict_ext['E_ext'].append(E)                      # E_ext
+    dict_ext['sigma_q_ext'].append(sigma_q)                # sigma_q_ext
+    dict_ext['LHF_ext'].append(q_flux_s*Le)            # LHF_ext
+    
+    return dh_dt, dq_dt, dth_dt, dU_dt, dV_dt
