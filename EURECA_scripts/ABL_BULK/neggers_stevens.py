a5512dedb8f981cdc361d5c1c6d4dcd114de9eac
meteorologopazzo <miscellaneasuper@gmail.com>
2025-02-10 22:47:20 +0100

    still starting ABL bulk

--- EURECA_scripts/ABL_BULK/neggers_stevens.py
+++ EURECA_scripts/ABL_BULK/neggers_stevens.py
@@ -128,6 +128,7 @@ def lcl(p,T,rh=None,rhl=None,rhs=None,return_ldl=False,return_min_lcl_ldl=False)
         else:
             rh = rhs
     if pv > p:
+        print(f"pv = {pv} - p={p}")
         return NA
 
     # Calculate lcl_liquid and lcl_solid
@@ -173,7 +174,7 @@ def prt_dyer74(zL):
 
     
     
-def neggers_et_al_2006_stevens_et_al_2002(t,y,SST,D,q_free,th_free,ps,f,U_free,V_free,we, dict_ext):
+def neggers_et_al_2006_stevens_et_al_2002(t,y,SST,D,q_free,th_free,ps,f,U_free,V_free,we, dict_ext, E_frac=1):
     """
     Subscript s denotes surface values, b stands for buoyancy, which is associated with the virtual potential
     temperature (theta_v), th indicates the liquid water potential temperature (theta_l) and q the total specific
@@ -217,15 +218,17 @@ def neggers_et_al_2006_stevens_et_al_2002(t,y,SST,D,q_free,th_free,ps,f,U_free,V
     # Entrainment.
     V_mag = np.sqrt(y[3]**2+y[4]**2) # [m/s], bulk wind speed.
     q_flux_s = V_mag*Cqs*(q_s-y[1]) # total specific humidity flux at the surface
+    q_flux_s_CC = V_mag*Cqs*(q_s) # total specific humidity flux at the surface
     th_flux_s = V_mag*Cths*(th_s-y[2]) # liquid water potential temperature flux at the surface
     thv_flux_s = (1+0.61*q_s)*th_flux_s + 0.61*th_s*q_flux_s # surface virtual potential temp flux, <w'theta_v'>    # Entrainment.
     delta_q = Cqc*(q_free-y[1])
     delta_th = Cthc*(th_free-y[2])
     delta_thv = delta_th + 0.61*(y[1]*delta_th + y[2]*delta_q + delta_q*delta_th)
     E = 0.2*thv_flux_s/delta_thv
+    
     # Mass flux.
     thv0 = y[2]*(1+0.61*y[1]) # [K], ABL virtual potential temperature
-    b_flux_s = g*thv_flux_s/thv0 # surface buoyancy flux
+    b_flux_s = g*thv_flux_s/thv0           # surface buoyancy flux
     w_star = (y[0]*b_flux_s)**(1/3) # m/s, Deardorff convective velocity scale. 5th try
 #    w_star = (g*y[0]*thv_flux_s/(y[2]*(ps/ref_p)**(Rd/cpd)*(1+0.61*y[1])))**(1/3) # m/s, Deardorff convective velocity scale. 4th try
 #    w_star = (g*y[0]*thv_flux_s/(y[2]*(1+0.61*y[1])))**(1/3) # m/s, Deardorff convective velocity scale. 3rd try
@@ -239,6 +242,7 @@ def neggers_et_al_2006_stevens_et_al_2002(t,y,SST,D,q_free,th_free,ps,f,U_free,V
     sigma_q = np.sqrt(-q_flux_s*delta_q*y[0]/(w_star*delta_z))
     area_c = 0.5 + beta*np.arctan(gamma*(y[1]-q_sat)/sigma_q)
     M = area_c*w_star
+    
     # Entrainment for the horizontal momentum.
     u_star = V_mag*np.sqrt(CD) # [m/s], Friction velocity, assuming air density equal to 1... We can correct this!
     L = - u_star**3/(0.4*b_flux_s)# [m], Monin-Obukhov length
@@ -246,6 +250,12 @@ def neggers_et_al_2006_stevens_et_al_2002(t,y,SST,D,q_free,th_free,ps,f,U_free,V
     zL = 10/L # [1], ratio z/L, we assume z=10 m as a reference height: we are interested in the surface stability
     we_dyn = E*prt_dyer74(zL)
 #     print(we_dyn)
+
+    # Surface air density.
+    rhos = ps*100/(Rd*T0)
+    
+    # check what happens to LCL
+    LCL = lcl(ps*100,T0,y[1]/q_s)
     
     ### Define the equations to be solved.
     dh_dt = E - D*y[0] - M 
@@ -260,11 +270,133 @@ def neggers_et_al_2006_stevens_et_al_2002(t,y,SST,D,q_free,th_free,ps,f,U_free,V
     #print(w_star)
     #print((y[1]-q_sat)/sigma_q)
     
-    dict_ext['area_c_ext'].append(area_c)                 # area_c_ext
-    dict_ext['w_star_ext'].append(w_star)                 # w_star_ext
-    dict_ext['M_ext'].append(M)                      # M_ext
-    dict_ext['E_ext'].append(E)                      # E_ext
-    dict_ext['sigma_q_ext'].append(sigma_q)                # sigma_q_ext
-    dict_ext['LHF_ext'].append(q_flux_s*Le)            # LHF_ext
+    dict_ext['area_c_ext'].append(area_c)                # area_c_ext
+    dict_ext['w_star_ext'].append(w_star)                # w_star_ext
+    dict_ext['M_ext'].append(M)                          # M_ext
+    dict_ext['E_ext'].append(E)                          # E_ext
+    dict_ext['sigma_q_ext'].append(sigma_q)              # sigma_q_ext
+    dict_ext['qsat_ext'].append(q_sat)                   # q sat at h
+    
+    dict_ext['LHF_ext'].append(q_flux_s*Le*rhos)         # LHF_ext
+    dict_ext['LHF_CC_ext'].append(q_flux_s_CC*Le*rhos)   # LHF_ext
+    dict_ext['SHF_ext'].append(th_flux_s*cpd*rhos)       # SHF_ext
+    
+    dict_ext['we_dyn_ext'].append(we_dyn)                # entrainment of momentum
+    dict_ext['LCL_ext'].append(LCL)                      # LCL height
+    
+    return dh_dt, dq_dt, dth_dt, dU_dt, dV_dt
+
+
+
+
+def neggers_stevens_FracE(t,y,SST,D,q_free,th_free,ps,f,U_free,V_free,we, dict_ext,E_frac):
+    """
+    Subscript s denotes surface values, b stands for buoyancy, which is associated with the virtual potential
+    temperature (theta_v), th indicates the liquid water potential temperature (theta_l) and q the total specific
+    humidity (q_t).
+    A bulk model of the wind speed is added starting from the Stevens et al., JCli (2002) paper
+    "Entrainment, Rayleigh friction, and boundary layer winds over the tropical Pacific"
+    y[3] is U, the zonal bulk wind component
+    y[4] is V, the meridional bulk wind component
+    V_mag is the magnitude of the bulk wind = np.sqrt(y[3]**2+y[4]**2)
+    """
+    # Define some constants.
+    delta_z = 100.0 # [m]
+    Cqs = 0.0012 # [1], surface exchange coefficient for q
+    Cths = 0.0012 # [1], surface exchange coefficient for theta
+    Cqc = 0.1 # [1], cloud level exchange coefficient for q
+    Cthc = 0.03 # [1], cloud level exchange coefficient for theta
+    Le = 2.5e6 # [J/kg], latent heat of vaporization 
+    cpd  = 1004.67 # [J/K/kg], dry air specific heat at constant pressure
+    Rd = 287.1 # [J/K/kg], dry air gas constant
+    # air density is supposed to be constant and equal to 1...
+    beta = 0.36
+    gamma = 1.55
+    g = 9.81 # [m/s2], acceleration due to gravity
+    CD = 1/900. # drag coefficient.
+    
+    ### Define some forcing variables for the BOMEX example -> this will be given as input parameters.
+    #V = 8.75 # [m/s], large scale horizontal wind
+    #SST = 300.4 # [K], sea surface temperature
+    #D = 4.3e-6 # [1/s], large scale divergence
+    #q_free = 4.0e-3 # [kg/kg] free tropospheric total mixing ratio
+    #th_free = 308 # [K], free tropospheric liquid virtual potential temperature
+    F_advq = -1.2e-3/86400 # [kg/kg/s]
+    F_advth = 0.0/86400 # [K/s]
+    F_rad = -2.0/86400 # [K/s]
+    #ps = 1015 # [hPa], surface pressure
+    ref_p = 1000 # [hPa], reference pressure for the potential temperature: is this the surface pressure????
+    q_s = qsea(SST-273.15,ps)*1e-3 # [kg/kg], surface total specific humidity (saturation value)
+    th_s = SST*(ref_p/ps)**(Rd/cpd) # [K], surface liquid water potential temperature, assuming no liquid water at the surface
+    
+    ### Define some diagnostic variables.
+    # Entrainment.
+    V_mag = np.sqrt(y[3]**2+y[4]**2) # [m/s], bulk wind speed.
+    q_flux_s = V_mag*Cqs*(q_s-y[1]) # total specific humidity flux at the surface
+    q_flux_s_CC = V_mag*Cqs*(q_s) # total specific humidity flux at the surface
+    th_flux_s = V_mag*Cths*(th_s-y[2]) # liquid water potential temperature flux at the surface
+    thv_flux_s = (1+0.61*q_s)*th_flux_s + 0.61*th_s*q_flux_s # surface virtual potential temp flux, <w'theta_v'>    # Entrainment.
+    delta_q = Cqc*(q_free-y[1])
+    delta_th = Cthc*(th_free-y[2])
+    delta_thv = delta_th + 0.61*(y[1]*delta_th + y[2]*delta_q + delta_q*delta_th)
+    E = 0.2*thv_flux_s/delta_thv
+    
+    # Mass flux.
+    thv0 = y[2]*(1+0.61*y[1]) # [K], ABL virtual potential temperature
+    b_flux_s = g*thv_flux_s/thv0           # surface buoyancy flux
+    w_star = (y[0]*b_flux_s)**(1/3) # m/s, Deardorff convective velocity scale. 5th try
+#    w_star = (g*y[0]*thv_flux_s/(y[2]*(ps/ref_p)**(Rd/cpd)*(1+0.61*y[1])))**(1/3) # m/s, Deardorff convective velocity scale. 4th try
+#    w_star = (g*y[0]*thv_flux_s/(y[2]*(1+0.61*y[1])))**(1/3) # m/s, Deardorff convective velocity scale. 3rd try
+#    w_star = (g*y[0]*thv_flux_s/y[2])**(1/3) # m/s, Deardorff convective velocity scale. 2nd try
+#    w_star = (g*y[0]*thv_flux_s/th_s)**(1/3) # m/s, Deardorff convective velocity scale. 1st try    
+    T0 = y[2]*(ps/ref_p)**(Rd/cpd) # [K], air temperature at the surface, from the ABL theta value.
+    T_h = T0-g/cpd*y[0] # [K], air temperature at h following a dry adiabat.
+    p_h = ps*100*(1-g*y[0]/(T0*cpd))**(cpd/Rd) # [Pa], air pressure at h with p=rho*R*T; dp/dz=-rho*g; dtheta/dz=0 
+    e_sat_h = qsat(T_h-273.15,p_h/100)*100 # [Pa], saturation vapor pressure at h.
+    q_sat = 0.622*e_sat_h/(p_h-0.378*e_sat_h) # [kg/kg], saturation specific humidity at h.   
+    sigma_q = np.sqrt(-q_flux_s*delta_q*y[0]/(w_star*delta_z))
+    area_c = 0.5 + beta*np.arctan(gamma*(y[1]-q_sat)/sigma_q)
+    M = area_c*w_star
+    
+    # Entrainment for the horizontal momentum.
+    u_star = V_mag*np.sqrt(CD) # [m/s], Friction velocity, assuming air density equal to 1... We can correct this!
+    L = - u_star**3/(0.4*b_flux_s)# [m], Monin-Obukhov length
+#     print
+    zL = 10/L # [1], ratio z/L, we assume z=10 m as a reference height: we are interested in the surface stability
+    we_dyn = E*prt_dyer74(zL)
+#     print(we_dyn)
+
+    # Surface air density.
+    rhos = ps*100/(Rd*T0)
+    
+    # check what happens to LCL
+    LCL = lcl(ps*100,T0,y[1]/q_s)
+    
+    ### Define the equations to be solved.
+    dh_dt = E - D*y[0] - M 
+    dq_dt = (q_flux_s + E_frac*E*delta_q)/y[0] + F_advq
+    dth_dt = (th_flux_s + E_frac*E*delta_th)/y[0] + F_advth + F_rad
+    dU_dt = f*(y[4]-V_free)-y[3]*(CD*V_mag+we_dyn)/y[0]+U_free*E_frac*we_dyn/y[0]
+    dV_dt = -f*(y[3]-U_free)-y[4]*(CD*V_mag+we_dyn)/y[0]+V_free*E_frac*we_dyn/y[0]
+    
+    #print(area_c)
+    #print((y[1]-q_sat)*1e3)
+    #print(sigma_q*1e3)
+    #print(w_star)
+    #print((y[1]-q_sat)/sigma_q)
+    
+    dict_ext['area_c_ext'].append(area_c)                # area_c_ext
+    dict_ext['w_star_ext'].append(w_star)                # w_star_ext
+    dict_ext['M_ext'].append(M)                          # M_ext
+    dict_ext['E_ext'].append(E)                          # E_ext
+    dict_ext['sigma_q_ext'].append(sigma_q)              # sigma_q_ext
+    dict_ext['qsat_ext'].append(q_sat)                   # q sat at h
+    
+    dict_ext['LHF_ext'].append(q_flux_s*Le*rhos)         # LHF_ext
+    dict_ext['LHF_CC_ext'].append(q_flux_s_CC*Le*rhos)   # LHF_ext
+    dict_ext['SHF_ext'].append(th_flux_s*cpd*rhos)       # SHF_ext
+    
+    dict_ext['we_dyn_ext'].append(we_dyn)                # entrainment of momentum
+    dict_ext['LCL_ext'].append(LCL)                      # LCL height
     
     return dh_dt, dq_dt, dth_dt, dU_dt, dV_dt
